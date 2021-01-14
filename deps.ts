2bfa5ce73395de7b17350e9ff0fe19b2923a4ba5
Noah Harris <mail@gastrodon.io>
2021-01-14 02:26:11 -0500

    Implement ferrothorn getting and posting

--- deps.ts
+++ deps.ts
@@ -5,3 +5,5 @@ export {
 } from "https://deno.land/std/testing/asserts.ts";
 
 export { v4 } from "https://deno.land/std/uuid/mod.ts";
+
+export { Image } from "https://deno.land/x/imagescript/ImageScript.js";
--- ferrothorn.ts
+++ ferrothorn.ts
@@ -1,12 +1,23 @@
 import { Image } from "./deps.ts";
 
-export const auth: string = Deno.env.get("FERROTHORN_AUTH") as string)
-export const host: string = (Deno.env.get("FERROTHORN_HOST") as string).replace(
+const auth: string = Deno.env.get("FERROTHORN_AUTH") as string;
+const host: string = (Deno.env.get("FERROTHORN_HOST") as string).replace(
   /\/$/,
   "",
 );
 
-export async function get_image(id: string): Promise<Image> {
-  const response: Response = await fetch(`${host}/`);
-  return new Image(4, 4);
+export async function get_image(id: string): Promise<Image | null> {
+  const response: Response = await fetch(`${host}/${id}`);
+  if (response.status == 404) {
+    return null;
+  }
+
+  return await Image.decode(await (await response.blob()).arrayBuffer());
+}
+
+export async function post_image(id: string, image: Image): Promise<Response> {
+  const form: FormData = new FormData();
+  form.append("file", new Blob([(await image.encode(3)).buffer]), "file.png");
+
+  return await fetch(`${host}/${id}`, { method: "POST", body: form });
 }
